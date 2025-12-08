bea72e4e23153e68488ed2f36d1f150636c889ee
Matouš Benedikt <matous.benedikt@gmail.com>
2025-12-08 20:52:39 +0100

    Add responsive teams page with team cards grid system
    Introduces a new teams section with visually enhanced team cards
    Improves navigation, and updates CSS for better mobile usability.
    Enhances user experience by making team information more accessible
    and visually appealing.

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // program pro zobrazeni menu pri zmacknuti hamburger ikony
-let hamburger = document.querySelector(".nav__hamburger");
-let menu = document.querySelector(".nav__menu");
+const hamburger = document.querySelector(".nav__hamburger");
+const menu = document.querySelector(".nav__menu");
+const body = document.body;
 
 hamburger.addEventListener("click", showMenu);
 
 function showMenu () {
     menu.classList.toggle("nav__menu--open");
     hamburger.classList.toggle("nav__cross");
+    body.classList.toggle("no-scroll");
 }
